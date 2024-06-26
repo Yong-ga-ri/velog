82b88e4f8be7c43f2a1e2ae2096cbca7ffb80622
Yong-ga-ri <rlfgks97@gmail.com>
2024-06-26 21:38:27 +0900

    update python script for logical

--- scripts/update_blog.py
+++ scripts/update_blog.py
@@ -2,6 +2,10 @@ import os
 import feedparser
 from datetime import datetime
 from git import Repo
+from dotenv import load_dotenv
+
+# 환경 변수 로드
+load_dotenv()
 
 # GitHub 액세스 토큰과 저장소 정보 설정
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
@@ -9,39 +13,40 @@ REPO_OWNER = os.getenv('REPO_OWNER')
 REPO_NAME = os.getenv('REPO_NAME')
 RSS_FEED_URL = 'https://v2.velog.io/rss/@rlfgks97'
 
-def fetch_posts():
-    # Velog의 RSS 피드에서 포스트 정보 가져오기
-    feed = feedparser.parse(RSS_FEED_URL)
-    posts = []
-    for entry in feed.entries:
-        post_title = entry.title
-        post_content = entry.description  # 또는 entry.content[0].value 사용 가능
-        post_updated = datetime(*entry.updated_parsed[:6])
-        posts.append((post_title, post_content, post_updated))
-    return posts
+
+# 'velog-posts' 폴더 경로
+posts_dir = os.path.join('.', 'velog-posts')
+
+# 'velog-posts' 폴더가 없다면 생성
+if not os.path.exists(posts_dir):
+    os.makedirs(posts_dir)
+
 
 def main():
-    posts = fetch_posts()
     repo = Repo('.')  # 현재 디렉토리의 Git 저장소를 로드
-
-    for post_title, post_content, post_updated in posts:
-        file_name = f"posts/{post_title.replace(' ', '_')}.md"
-        file_path = os.path.join(repo.working_tree_dir, file_name)
-
-        # 파일 생성 또는 업데이트
-        if os.path.exists(file_path):
-            with open(file_path, 'w', encoding='utf-8') as file:
-                file.write(post_content)
-            repo.index.add([file_name])
-            repo.index.commit(f"Update post: {post_title}")
-        else:
-            with open(file_path, 'w', encoding='utf-8') as file:
-                file.write(post_content)
-            repo.index.add([file_name])
-            repo.index.commit(f"Add post: {post_title}")
-
-    # GitHub에 푸시
     origin = repo.remote(name='origin')
+
+    # Velog의 RSS 피드에서 포스트 정보 가져오기
+    feed = feedparser.parse(RSS_FEED_URL)
+    for entry in feed.entries:
+        post_title = entry.title
+        post_title = post_title.replace('/', '-')
+        post_title = post_title.replace('\\', '-')
+        post_title += '.md'
+        date = datetime(*entry.updated_parsed[:6])
+        file_path = os.path.join(posts_dir, post_title)
+
+        # 파일이 이미 존재하지 않으면 생성
+        if not os.path.exists(file_path):
+            with open(file_path, 'w', encoding='utf-8') as file:
+                file.write(entry.description)  # 글 내용을 파일에 작성
+
+            # 깃허브 커밋
+            repo.git.add(file_path)
+            repo.git.commit('-m', f'add title:{entry.title} updated at {date}')
+    
+    # 푸시 설정
+    origin.set_url(f'https://{GITHUB_TOKEN}@github.com/{REPO_OWNER}/{REPO_NAME}.git')
     origin.push()
 
 if __name__ == "__main__":
